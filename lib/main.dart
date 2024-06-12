60e19649823d8efac9d81f95ae27fc61942b190c
kimhyungmin <jeen14rla@gmail.com>
2024-06-12 17:51:09 +0900

    feat: add changeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_stopwatch_app/presentation/stopwatch_screen.dart';
+import 'package:flutter_stopwatch_app/presentation/stopwatch_screen_ver_view_model.dart';
+import 'package:flutter_stopwatch_app/presentation/stopwatch_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +23,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const StopWatchScreen(),
+      home: ChangeNotifierProvider(
+        create: (_)=> StopWatchViewModel(),
+        child: const StopWatchScreen(),
+      ),
     );
   }
 }
